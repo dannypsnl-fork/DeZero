526a0a34bd5db2935069668591ab43140583b440
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2021-05-22 04:14:34 +0800

    drop middle grad to improve performance

--- dezero.py
+++ dezero.py
@@ -17,7 +17,7 @@ class Variable:
         self.creator = func
         self.generation = func.generation + 1
 
-    def backward(self):
+    def backward(self, retain_grad=False):
         if self.grad is None:
             self.grad = np.ones_like(self.data)
 
@@ -47,6 +47,10 @@ class Variable:
 
                 if x.creator is not None:
                     add_func(x.creator)
+
+            if not retain_grad:
+                for y in f.outputs:
+                    y().grad = None
 
     def cleargrad(self):
         self.grad = None
