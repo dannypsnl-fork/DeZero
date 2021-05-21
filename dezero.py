ae6dc0b3134100e80a6c8f5ed1d7de7b5defbd0b
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2021-05-22 04:12:58 +0800

    weakref

--- dezero.py
+++ dezero.py
@@ -1,4 +1,5 @@
 import numpy as np
+import weakref
 
 
 class Variable:
@@ -33,7 +34,7 @@ class Variable:
 
         while funcs:
             f = funcs.pop()
-            gys = [output.grad for output in f.outputs]
+            gys = [output().grad for output in f.outputs]
             gxs = f.backward(*gys)
             if not isinstance(gxs, tuple):
                 gxs = (gxs,)
@@ -69,8 +70,7 @@ class Function:
         for output in outputs:
             output.set_creator(self)
         self.inputs = inputs
-        self.outputs = outputs
-
+        self.outputs = [weakref.ref(output) for output in outputs]
         return outputs if len(outputs) > 1 else outputs[0]
 
     def forward(self, xs):
