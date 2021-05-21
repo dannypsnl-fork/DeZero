8c1078492954bef70e53b021182e89ad879556ee
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2021-05-22 04:23:03 +0800

    len and print

--- dezero.py
+++ dezero.py
@@ -18,12 +18,13 @@ class Config:
 
 
 class Variable:
-    def __init__(self, data):
+    def __init__(self, data, name=None):
         if data is not None:
             if not isinstance(data, np.ndarray):
                 raise TypeError('{} is not supported'.format(type(data)))
 
         self.data = data
+        self.name = name
         self.grad = None
         self.creator = None
         self.generation = 0
@@ -69,6 +70,31 @@ class Variable:
 
     def cleargrad(self):
         self.grad = None
+
+    def __len__(self):
+        return len(self.data)
+
+    def __repr__(self):
+        if self.data is None:
+            return 'variable(None)'
+        p = str(self.data).replace('\n', '\n' + ' ' * 9)
+        return 'variable(' + p + ')'
+
+    @property
+    def shape(self):
+        return self.data.shape
+
+    @property
+    def ndim(self):
+        return self.data.ndim
+
+    @property
+    def size(self):
+        return self.data.size
+
+    @property
+    def dtype(self):
+        return self.data.dtype
 
 
 def as_array(x):
