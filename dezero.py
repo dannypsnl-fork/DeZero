c924af1d8820c8f044a09c24f11dd3249dfa7a41
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2021-05-22 04:19:29 +0800

    context

--- dezero.py
+++ dezero.py
@@ -1,5 +1,20 @@
 import numpy as np
 import weakref
+import contextlib
+
+
+@contextlib.contextmanager
+def using_config(name, value):
+    old_value = getattr(Config, name)
+    setattr(Config, name, value)
+    try:
+        yield
+    finally:
+        setattr(Config, name, old_value)
+
+
+class Config:
+    enable_backprop = True
 
 
 class Variable:
@@ -70,11 +85,13 @@ class Function:
             ys = (ys,)
         outputs = [Variable(as_array(y)) for y in ys]
 
-        self.generation = max([x.generation for x in inputs])
-        for output in outputs:
-            output.set_creator(self)
-        self.inputs = inputs
-        self.outputs = [weakref.ref(output) for output in outputs]
+        if Config.enable_backprop:
+            self.generation = max([x.generation for x in inputs])
+            for output in outputs:
+                output.set_creator(self)
+            self.inputs = inputs
+            self.outputs = [weakref.ref(output) for output in outputs]
+
         return outputs if len(outputs) > 1 else outputs[0]
 
     def forward(self, xs):
