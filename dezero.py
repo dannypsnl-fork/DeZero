5ebc08360aa609b5a4aa5ba54a5c2a82e3d78dc7
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2021-06-11 07:39:25 +0800

    step 22

--- dezero.py
+++ dezero.py
@@ -192,6 +192,84 @@ def mul(x0, x1):
     return Mul()(x0, x1)
 
 
+class Neg(Function):
+    def forward(self, x):
+        return -x
+
+    def backward(self, gy):
+        return -gy
+
+
+def neg(x):
+    return Neg()(x)
+
+
+class Sub(Function):
+    def forward(self, x0, x1):
+        y = x0-x1
+        return y
+
+    def backward(self, gy):
+        return gy, -gy
+
+
+def sub(x0, x1):
+    x1 = as_array(x1)
+    return Sub()(x0, x1)
+
+
+def rsub(x0, x1):
+    x1 = as_array(x1)
+    return Sub()(x1, x0)
+
+
+class Div(Function):
+    def forward(self, x0, x1):
+        y = x0/x1
+        return y
+
+    def backward(self, gy):
+        x0, x1 = self.inputs[0].data, self.inputs[1].data
+        gx0 = gy/x1
+        gx1 = gy*(-x0 / x1**2)
+        return gx0, gx1
+
+
+def div(x0, x1):
+    x1 = as_array(x1)
+    return Div()(x0, x1)
+
+
+def rdiv(x0, x1):
+    x1 = as_array(x1)
+    return Div()(x1, x0)
+
+
+class Pow(Function):
+    def __init__(self, c):
+        self.c = c
+
+    def forward(self, x):
+        y = x**self.c
+        return y
+
+    def backward(self, gy):
+        x = self.inputs[0].data
+        c = self.c
+        gx = c*x**(c-1) * gy
+        return gx
+
+
+def pow(x, c):
+    return Pow(c)(x)
+
+
+Variable.__pow__ = pow
+Variable.__truediv__ = div
+Variable.__rtruediv__ = rdiv
+Variable.__sub__ = sub
+Variable.__rsub__ = rsub
+Variable.__neg__ = neg
 Variable.__mul__ = mul
 Variable.__rmul__ = mul
 Variable.__add__ = add
