e567a39846fec7d0822b88e64ef6c6cef1805477
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2021-06-11 08:19:33 +0800

    comment

--- dezero/__init__.py
+++ dezero/__init__.py
@@ -1,8 +1,10 @@
+# export
 from .core import Variable
 from .core import Function
 from .core import as_array
-from .functions import setup_functions
 from .functions import square
 from .functions import exp
 
+# setup
+from .functions import setup_functions
 setup_functions()
